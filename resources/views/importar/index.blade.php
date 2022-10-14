219e1050374b569affef6977f4e15cb51a0f9ea3
josel19 <jjlugomez123@gmail.com>
2022-10-14 15:35:03 -0500

    proyecto a medias

---
+++ resources/views/importar/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('title')
+Importar
+@endsection
+
+
+@section('content')
+<div class="container-fluid">
+<h1 class="text-center">Importar</h1>
+</div>
+@endsection
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,72 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <div class="sidebar-brand d-flex align-items-center justify-content-start" style="background-color: white;">
+        <div class="sidebar-brand-icon" >
+            <img src="img/IRCicon 1.png" class="img-fluid" width="170" height="50">
+        </div>
+    </div>
+
+    <!-- Divider -->
+
+
+    <!-- Nav Item - Dashboard -->
+    {{-- importar item --}}
+    <li class="nav-item active items m-t-5">
+        <a class="nav-link" href="index.html">
+            <div class="d-flex flex-column ">
+                <i  class="fa-solid fa-cloud-arrow-up text-center"></i>
+                <div Class="text-center"><span>Importar</span></div>
+            </div>
+            <hr class="sidebar-divider">
+        </a>
+    </li>
+
+    <li class="nav-item  items m-t-5">
+        <a class="nav-link" href="index.html">
+            <div class="d-flex flex-column ">
+                <i class="fa-solid fa-pencil text-center"></i>
+                <div Class="text-center"><span>Administrar<br>Proceso</span></div>
+            </div>
+            <hr class="sidebar-divider">
+        </a>
+    </li>
+    <li class="nav-item  items m-t-5">
+        <a class="nav-link" href="index.html">
+            <div class="d-flex flex-column ">
+                <i class="fa-solid fa-house-medical-circle-check text-center"></i>
+                <div Class="text-center"><span>Gestionar</span></div>
+            </div>
+            <hr class="sidebar-divider">
+        </a>
+    </li>
+    <li class="nav-item  items m-t-5">
+        <a class="nav-link" href="index.html">
+            <div class="d-flex flex-column ">
+                <i  class="fa-solid fa-cloud-arrow-up text-center"></i>
+                <div Class="text-center"><span>Consultar<br>pacientes</span></div>
+            </div>
+            <hr class="sidebar-divider">
+        </a>
+    </li>
+    <li class="nav-item  items m-t-5">
+        <a class="nav-link" href="index.html">
+            <div class="d-flex flex-column ">
+                <i  class="fa-solid fa-cloud-arrow-up text-center"></i>
+                <div Class="text-center"><span>Administracion</span></div>
+            </div>
+            <hr class="sidebar-divider">
+        </a>
+    </li>
+    <li class="nav-item  items m-t-5">
+        <a class="nav-link" href="index.html">
+            <div class="d-flex flex-column ">
+                <i  class="fa-solid fa-cloud-arrow-up text-center"></i>
+                <div Class="text-center"><span>Reportes</span></div>
+            </div>
+            <hr class="sidebar-divider">
+        </a>
+    </li>
+
+
+</ul>
