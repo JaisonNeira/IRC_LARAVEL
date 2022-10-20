49a1aecf38a205ac7c1b33a394a14fcd75c299f0
josel19 <jjlugogomez123@gmail.com>
2022-10-19 21:57:44 -0500

    validaciones import listas

--- public/js/funcionalidades/validator.js
+++ public/js/funcionalidades/validator.js
@@ -2,22 +2,19 @@
 function validacion(){
     var todo_ok = true;
     var seleccion = document.getElementById('seleccion');
-    var importar = document.getElementById('a');
+    var importar = document.getElementById('add_archivo');
 
-    if(seleccion.value == 'Tipo de proceso' || importar.value == 'Selecciona un archivo'){
-        console.log(importar.files.length);
+    if(seleccion.value == 'Tipo de proceso' || importar.files.length == 0){
         if(seleccion.value == 'Tipo de proceso'){
-             document.getElementById('a').style.border = '3px solid #E22A3D';
-             document.getElementById('a').style.borderRadius = '4px 4px 4px 4px';
-        }else
-             document.getElementById('a').style.border = '3px solid #E22A3D';
-             document.getElementById('a').style.borderRadius = '4px 4px 4px 4px';
-
-        if(importar.importar.value == 'Selecciona un archivo'){
-            document.getElementById('imp').style.border = '3px solid #E22A3D';
-            document.getElementById('imp').style.borderRadius = '4px 4px 4px 4px';
+             document.getElementById('select').style.borderRadius = '4px 4px 4px 4px';
+             document.getElementById('select').style.boxShadow= '0px 0px 4px 4px rgba(255, 0, 168, 1)';
+             document.getElementById('error1').style.display= 'flex';
         }
-       alert('Campos sin llenar');
+        if(importar.files.length == 0){
+          document.getElementById('validator').style.boxShadow= '0px 0px 4px 4px rgba(255, 0, 168, 1)';
+          document.getElementById('validator2').style.boxShadow= '0px 0px 4px 4px rgba(255, 0, 168, 1)';
+          document.getElementById('error2').style.display= 'flex';
+        }
       return todo_ok = false;
    }
 }
--- resources/views/importar/index.blade.php
+++ resources/views/importar/index.blade.php
@@ -4,43 +4,48 @@
     Importar
 @endsection
 
-@section('script')
-<script src="{{ asset('js/funcionalidades/validator.js') }}"></script>
-@endsection
+
 
 
 @section('content')
     <div class="container" style="width: 55%; align:left;">
-        <form name="form_a" onsubmit="return validacion()">
+        {{--  Inicio Formulorio  --}}
+        <form name="form" onsubmit="return validacion()">
             <h1 style="font-weight: bold;">Selecciona el tipo de proceso</h1>
-            <div class="select  col-12 shadow-sm mb-4 align-items-center" id="a">
+            <div class="select  col-12  align-items-center" id="select">
                 <select id="seleccion" style="font-weight: bold;">
                     <option selected disabled>Tipo de proceso</option>
-                    <option value="1" style="color: #000000">Inasistidos</option>
-                    <option value="2" style="color: #000000">Seguimiento demanda inducida</option>
-                    <option value="3" style="color: #000000">Recordatorios</option>
-                    <option value="4" style="color: #000000">Hospitalizados</option>
-                    <option value="5" style="color: #000000">Brigadas</option>
-                    <option value="6" style="color: #000000">Reprogramacion</option>
+                    <option value="1" class="opciones">Inasistidos</option>
+                    <option value="2" class="opciones">Seguimiento demanda inducida</option>
+                    <option value="3" class="opciones">Recordatorios</option>
+                    <option value="4" class="opciones">Hospitalizados</option>
+                    <option value="5" class="opciones">Brigadas</option>
+                    <option value="6" class="opciones">Reprogramacion</option>
                 </select>
             </div>
-            <div class="col-12  mb-4 align-items-center" style="padding-right: 0; padding-left: 0;">
-                <input type="file" class="irc-input-file" id="add_archivo"
-                    accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel">
-                <label for="imp" id="validator">
-                    <span class="irc-input-file_nombre">
-                        <span id="a">Selecciona un archivo</span>
+            <span class="error" id="error1">Selecciona un proceso</span> {{--error--}}
+            <div class="col-12 mt-4 mb-4 align-items-center" style="padding-right: 0; padding-left: 0;">
+                {{-- Input file --}}
+                <input type="file" class="irc-input-file" id="add_archivo" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel">
+                <label for="add_archivo">
+                    <span class="irc-input-file_nombre" id="validator">
+                        <span>Selecciona un archivo</span>
                     </span>
-                    <span class="irc-input-file_boton">
+                    <span class="irc-input-file_boton" id="validator2">
                         <i class="fa-solid fa-paperclip text-center m-2" style="font-size: 30px;"></i>
                     </span>
-                    <div id="alert1"></div>
                 </label>
+                <span class="error" id="error2">Ingresa un archivo</span>{{--error--}}
             </div>
             <div>
-                <button type="submit" class="btn btn-primary mt-3"
-                    style="width: 200px; height: 48px; font-weight: bold; border: 1px solid  #E22A3D; color: #fff ">Subir
-                    Archivo</button>
+            <button type="submit" class="btn btn-primary mt-3"
+            style="width: 200px; height: 48px; font-weight: bold; border: 1px solid  #E22A3D; color: #fff ">Subir
+            Archivo</button>
         </form>
+          {{--  Fin Formulorio  --}}
     </div>
+@endsection
+
+@section('script')
+<script src="{{ asset('js/funcionalidades/validator.js') }}"></script>
 @endsection
