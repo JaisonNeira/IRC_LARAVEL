b5f3430657c1c692a92aa4cfb0458843b6748ad9
josel19 <jjlugomez123@gmail.com>
2022-11-03 11:34:16 -0500

    home oc

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,22 @@
 @endsection
 
 @section('content')
-    <div class="container-fluid text-center" >
-        <div class="container shadow-lg text-center bg-img-color rounded" style="min-height: 500px;">
-           
-            <div class="text-left text-white">
-                <h1 style="font-weight: bold;" class="p-5">Bienvenido, {{ Auth::user()->name }} </h1>
+    <div class="container-fluid text-center">
+        <div class="container  shadow-lg text-center bg-img-color rounded" style="min-height: 500px;">
+            <div class="row">
+                <div class="col-6 text-left p-2">
+                    <img src="{{ asset('img/irc_icon_white.png') }}" alt="" style="max-width: 200px; max-height: 35px;">
+                </div>
+                <div class="col-6 text-right p-2">
+                    <img src="{{ asset('img/contacta_white.png') }}" alt="" style="max-width: 200px; max-height: 40px;">
+                </div>
+        
+                <div class="col-12 text-center text-white pt-5 mt-5">
+                    <h1 style="font-weight: bold;" class="pt-5">Bienvenido a IRC</h1>
+                    <h3 style="font-weight: bold;" class="">{{ Auth::user()->name }}</h3>
+                </div>
             </div>
         </div>
+    </div>
     </div>
 @endsection
