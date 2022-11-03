d9ee42c07315c4722eeedbf2c51c3b2dc496cdf5
Jaison Neira <Estudiosjaison@gmail.com>
2022-11-03 10:54:04 -0500

    x

--- resources/views/layouts/msj.blade.php
+++ resources/views/layouts/msj.blade.php
@@ -3,7 +3,9 @@
 @if(Session::has('mSucces'))
 <div class="alert alert-success alert-dismissible fade show" role="alert">
     <strong> {{ Session::get('mSucces') }} </strong>
-    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
 </div>
 @endif
 
@@ -11,7 +13,9 @@
 @if(Session::has('mDanger'))
 <div class="alert alert-danger alert-dismissible fade show" role="alert">
     <strong> {{ Session::get('mDanger') }} </strong>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
 </div>
 @endif
 
@@ -19,7 +23,9 @@
 @if(Session::has('mWarning'))
 <div class="alert alert-warning alert-dismissible fade show" role="alert">
     <strong> {{ Session::get('mWarning') }} </strong>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
 </div>
 @endif
 
