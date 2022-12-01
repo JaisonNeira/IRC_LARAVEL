7c13f043655f686b451b6dd35ca8b181864e4a13
Jaison Neira <Estudiosjaison@gmail.com>
2022-12-01 11:05:56 -0500

    reporte personalizado HOS

--- app/Http/Controllers/ReportePersonalizadoController.php
+++ app/Http/Controllers/ReportePersonalizadoController.php
@@ -10,6 +10,7 @@ use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\Mail;
 
 use App\Exports\InasistidosExport;
+use App\Exports\HospitalizadosExport;
 
 use App\Models\tipos_proceso;
 use App\Models\departamento;
@@ -61,10 +62,27 @@ class ReportePersonalizadoController extends Controller
             return Excel::download(new InasistidosExport($ina_data), 'INA_'.$fecha_ini.'_'.$fecha_fin.'_REPORTE.xlsx');
 
         }else if($tpp_id == 4){
-            dd('hospitalizados');
 
+            $sql_pacientes = "SELECT hos.hos_id, hos.hos_diagnostico, hos.hos_fecha_ingreso,
+            hos.hos_fecha_egreso, hos.hos_programa, hos.hos_pertenece_irc, pro.*
+            FROM cargues AS car
+            INNER JOIN procesos AS pro ON pro.car_id = car.car_id
+            INNER JOIN pacientes AS pac ON pac.pac_id = pro.pac_id
+            INNER JOIN hospitalizados AS hos ON hos.pro_id = pro.pro_id
+            WHERE car.car_estado = 1
+            AND car.tpp_id = ".$tpp_id."
+            AND pac.dep_id = ".$dep_id."
+            AND car.created_at BETWEEN '".$fecha_ini."' AND '".$fecha_fin."'";
+
+            $pacientes = DB::select($sql_pacientes);
+
+            if(count($pacientes) == 0){
+                return back()->with('mDanger', 'No hay pacientes entre el '.$fecha_ini.' y el '.$fecha_fin.'!');
+            }
+
+            $hos_data = $this->proceso_hostipalizados($tpp_id, $dep_id, $fecha_ini, $fecha_fin);
             
-
+            return Excel::download(new HospitalizadosExport($hos_data), '_'.$fecha_ini.'_'.$fecha_fin.'_REPORTE.xlsx');
         }else{
             dd('Erro');
         }
@@ -85,7 +103,7 @@ class ReportePersonalizadoController extends Controller
         AND car.created_at BETWEEN '".$fecha_ini."' AND '".$fecha_fin."'";
 
         $pacientes = DB::select($sql_pacientes);
-
+        $data[] = array();
         $l2 = 0;
         for ($l=0; $l < count($pacientes); $l++) {
 
@@ -139,7 +157,7 @@ class ReportePersonalizadoController extends Controller
             $registro["SEGUIMIENTO 3"] = " ";
             $registro["FECHA DE SEGUIMIENTO 3"] = " ";
 
-            if($v != 0){
+            if($v > -1){
                 for ($i=1; $i < count($gestiones)+1; $i++) {
 
                     $registro["SEGUIMIENTO ".$i] = $gestiones[$v]->tge_nombre;
@@ -150,7 +168,7 @@ class ReportePersonalizadoController extends Controller
 
             $registro["MOTIVO DE INASISTENCIA"] = $pacientes[$l2]->tin_nombre;
 
-            $data[] = $registro;
+            $data[$l2] = $registro;
 
             /* dd($data, $registro, $pacientes, $sql_gestiones); */
 
@@ -161,4 +179,111 @@ class ReportePersonalizadoController extends Controller
 
     }
 
+    public function proceso_hostipalizados($tpp_id, $dep_id, $fecha_ini, $fecha_fin){
+
+        $sql_procesos = "SELECT hos.hos_id, hos.hos_diagnostico, hos.hos_fecha_ingreso,
+            hos.hos_fecha_egreso, hos.hos_programa, hos.hos_pertenece_irc, car.car_fecha_reporte, car.car_mes, pro.*
+            FROM cargues AS car
+            INNER JOIN procesos AS pro ON pro.car_id = car.car_id
+            INNER JOIN pacientes AS pac ON pac.pac_id = pro.pac_id
+            INNER JOIN hospitalizados AS hos ON hos.pro_id = pro.pro_id
+            WHERE car.car_estado = 1
+            AND car.tpp_id = ".$tpp_id."
+            AND pac.dep_id = ".$dep_id."
+            AND car.created_at BETWEEN '".$fecha_ini."' AND '".$fecha_fin."'";
+
+        $procesos = DB::select($sql_procesos);
+        $data[] = array();
+        $h1 = 0;
+
+        for ($i=0; $i < count($procesos); $i++) {
+
+            $pac_id = $procesos[$h1]->pac_id;
+
+            $paciente = DB::select("SELECT tip.tip_alias, dep.dep_nombre, mun.mun_nombre, pac.*
+            FROM pacientes AS pac
+            INNER JOIN tipos_identificaciones AS tip ON pac.tip_id = tip.tip_id
+            INNER JOIN departamentos AS dep ON dep.dep_id = pac.dep_id
+            INNER JOIN municipios AS mun ON mun.mun_id = pac.mun_id
+            WHERE pac_id = ".$pac_id);
+
+            $registro = [
+                "Documento" => $paciente[0]->tip_alias,
+                "Numero_de_documento" => $paciente[0]->pac_identificacion,
+                "Primer_nombre" => $paciente[0]->pac_primer_nombre,
+                "Segundo_nombre" => $paciente[0]->pac_segundo_nombre,
+                "Primer_apellido" => $paciente[0]->pac_primer_apellido,
+                "Segundo_apellido" => $paciente[0]->pac_segundo_apellido,
+                "Telefono" => $paciente[0]->pac_telefono,
+                "Fecha_nacimiento" => $paciente[0]->pac_fecha_nacimiento,
+                "Departamento" => $paciente[0]->dep_nombre,
+                "Municipio" => $paciente[0]->mun_nombre,
+                "Direccion" => $paciente[0]->pac_direccion,
+                "Sexo" => $paciente[0]->pac_sexo,
+                "Regimen_afiliacion_SGSS" => $paciente[0]->pac_regimen_afiliacion_SGSS,
+                "Fecha_reporte" => $procesos[$h1]->car_fecha_reporte,
+                "Mes" => $procesos[$h1]->car_mes,
+                "Prioridad" => $procesos[$h1]->pro_prioridad,
+                "Diagnostico" => $procesos[$h1]->hos_diagnostico,
+                "Fecha_ingreso" => $procesos[$h1]->hos_fecha_ingreso,
+                "Fecha_egreso" => $procesos[$h1]->hos_fecha_egreso,
+                "Programa" => $procesos[$h1]->hos_programa,
+                "Pertenece_irc" => $procesos[$h1]->hos_pertenece_irc
+            ];
+
+            $pro_id = $procesos[$h1]->pro_id;
+
+            $sql_gestiones = "SELECT tge.tge_nombre , ges.*
+            FROM gestiones AS ges
+            INNER JOIN procesos AS pro ON pro.pro_id = ges.pro_id
+            INNER JOIN hospitalizados AS hos ON hos.pro_id = pro.pro_id
+            INNER JOIN tipos_gestiones AS tge ON tge.tge_id = ges.tge_id
+            WHERE ges.ges_estado = 1
+            AND ges.pro_id = ".$pro_id."
+            ORDER BY ges.ges_fecha DESC
+            LIMIT 3";
+
+            $gestiones = DB::select($sql_gestiones);
+
+            $v = count($gestiones)-1;
+
+            $registro["fecha de seguimiento 1"] = " ";
+            $registro["resultado de la gestion 1"] = " ";
+            $registro["fecha de seguimiento 2"] = " ";
+            $registro["resultado de la gestion 2"] = " ";
+            $registro["fecha de seguimiento 3"] = " ";
+            $registro["resultado de la gestion 3"] = " ";
+
+            if($v > -1){
+                for ($i2=1; $i2 < count($gestiones)+1; $i2++) {
+
+                    $registro["fecha de seguimiento ".$i2] = $gestiones[$v]->tge_nombre;
+                    $registro["resultado de la gestion ".$i2] = $gestiones[$v]->ges_fecha;
+                    $v -= 1;
+                }
+            }
+
+            $ges_id = $procesos[$h1]->ges_id;
+
+            if($ges_id != null){
+                $sql_g = "SELECT *
+                FROM gestiones
+                WHERE ges_id = ".$ges_id;
+
+                $ultima_gestion = DB::select($sql_g);
+
+                $registro["motivo de hospitalizacion"] = $ultima_gestion[0]->ges_comentario;
+            }else{
+                $registro["motivo de hospitalizacion"] = " ";
+            }
+
+
+            $data[$h1] = $registro;
+
+            $h1 += 1;
+        }
+
+        return $data;
+
+    }
 }
